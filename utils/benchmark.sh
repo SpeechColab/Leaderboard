b70e10d36ee7e9259fa038713361b4f4ca665c54
dophist <jerry.jiayu.du@gmail.com>
2021-02-17 22:20:34 +0800

    benchmarking pipeline almost done

--- utils/benchmark.sh
+++ utils/benchmark.sh
@@ -1,27 +1,89 @@
 #!/usr/bin/env bash
-cd /app/speechio/leaderboard/test_env
-echo $PWD
-
-service_list=$(cat submission_key)
-testset_list=$(cat test_sets)
 max_num_utts=10
 #----------------------------
-export TEST_ENV=$(readlink -f .)
+LEADERBOARD=/app/speechio/leaderboard
+cd $LEADERBOARD/test_env && echo $PWD
 
-date=`date +%Y%m%d`
-result=${TEST_ENV}/result
-database=${TEST_ENV}/dataset
-for testset in $testset_list; do
-    for service in $service_list; do
-        #cd service/$service
-        echo "==>Testing Service:$service TEST_SET:$testset DATE:$date NUM_UTTS:$max_num_utts"
-        dir=${date}__${service}__${testset}__${max_num_utts}
-        mkdir -p $dir
-        echo "MBI" > $dir/log
-        # nohup ${TEST_ENV}/utils/test.sh $database/$testset $dir $max_num_utts >& $dir/log &
-        sleep 0.5
-        #cd -
-    done
-    wait
+for testset in $(cat test_sets); do
+    date=$(date +%Y%m%d)
+    echo "==>Testing TEST_SET:$testset DATE:$date NUM_UTTS:$max_num_utts"
+    dir=${date}__${testset}__${max_num_utts}
+    mkdir -p $dir
+
+    #nohup ${LEADERBOARD}/utils/test.sh $database/$testset $dir $max_num_utts >& $dir/log &
+
+    testset=$(readlink -f ${LEADERBOARD}/dataset/$testset)
+    n=$(wc -l ${testset}/wav.scp | awk '{print$1}')
+    if [ $n -gt $max_num_utts ]; then
+        n=$max_num_utts
+    fi
+
+    stage=0
+    if [ ! -f ${testset}/wav.scp ] || [ ! -f ${testset}/trans.txt ]; then
+        echo "ERROR: missing wav.scp or trans.txt in test set"
+        exit 1;
+    fi
+
+    if [ $stage -le 1 ]; then
+        echo "$0: preparing test set from $testset to $dir"
+        awk -v d=$testset '{print $1"\t"d"/"$2}' ${testset}/wav.scp | head -n $n > $dir/wav.scp
+        awk '{print $1}' $dir/wav.scp > $dir/keys
+
+        cat /dev/null > $dir/trans.txt
+        for key in `cat $dir/keys`; do
+            grep -- "$key[[:space:]]" ${testset}/trans.txt >> $dir/trans.txt
+        done
+
+        num_wavs=`cat $dir/wav.scp | wc -l`
+        num_trans=`cat $dir/trans.txt | wc -l`
+        if [ $num_wavs != $num_trans ]; then
+            echo "ERROR, found wavs without trans, wavs:$num_wavs, trans:$num_trans"
+            exit -1
+        else
+            echo "  wavs:$num_wavs, trans:$num_trans"
+            echo -e "Done.\n"
+        fi
+    fi
+
+    if [ $stage -le 2 ]; then
+        echo "$0: recognizing ... log in $dir/log.MBI"
+        if [ -f 'MBI' ]; then
+            chmod +x MBI
+            ./MBI $dir/wav.scp $dir >& $dir/log.MBI
+            echo -e "Done.\n"
+        else
+            echo "ERROR: cannot find MBI program id $dir"
+        fi
+    fi
+
+    # if [ $stage -le 3 ]; then
+    #     echo "$0: computing WER/CER ... in $dir/log.compute_cer"
+    #     echo "-- <preparing reference"
+    #     python3 ${LEADERBOARD}/utils/cn_tn.py --has_key --to_upper $dir/trans.txt $dir/tmp.ref_tn.txt  # TN
+    #     python3 ${LEADERBOARD}/utils/split_to_char.py $dir/tmp.ref_tn.txt $dir/ref.txt
+    #     echo -e "-- done>\n"
+
+    #     echo "-- <preparing recognition text"
+    #     python3 ${LEADERBOARD}/utils/cn_tn.py --has_key --to_upper $dir/raw_rec.txt $dir/tmp.rec_tn.txt
+    #     python3 ${LEADERBOARD}/utils/split_to_char.py $dir/tmp.rec_tn.txt $dir/rec.txt
+    #     grep -v $'\t$' $dir/rec.txt > $dir/rec_present.txt # filter away empty recognition result
+    #     rm $dir/tmp.*
+    #     echo -e "-- done>\n"
+
+    #     echo "-- <computing CER and text alignment"
+    #     $COMPUTE_WER --mode=present --text=true ark,t:$dir/ref.txt ark,t:$dir/rec.txt > $dir/CER
+    #     $ALIGN_TEXT ark,t:$dir/ref.txt ark,t:$dir/rec.txt ark,t:$dir/align.txt
+    #     echo ""
+    #     $COMPUTE_WER --mode=present --text=true ark,t:$dir/ref.txt ark,t:$dir/rec_present.txt > $dir/CER_present
+    #     $ALIGN_TEXT ark,t:$dir/ref.txt ark,t:$dir/rec_present.txt ark,t:$dir/align_present.txt
+    #     echo -e "-- done>\n"
+
+    #     echo "-- <Getting pretty alignment"
+    #     ${LEADERBOARD}/utils/prettify_align.py $dir/align_present.txt $dir/CHECK.txt
+    #     echo -e "-- done>\n"
+    #     echo -e "Done.\n"
+    # fi
+
+    sleep 1
 done
 echo "Done"
