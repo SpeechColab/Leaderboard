200a5f1ba7e17bef3612002c0f2ea3ae5e79e786
dophist <jerry.ejwt@qq.com>
2021-04-05 08:55:31 +0800

    minor bug fixes

--- leaderboard_run_local.sh
+++ leaderboard_run_local.sh
@@ -2,5 +2,6 @@ model=speechio_kaldi
 leaderboard=$(readlink -f .)
 test_sets="MINI SPEECHIO_ASR_ZH00000"
 test_lang=zh
+rm test_env
 ln -s models/$model test_env
-LEADERBOARD=$leaderboard TEST_SETS="$test_sets" TEST_LANG=$test_lang sh utils/benchmark.sh
+LEADERBOARD=$leaderboard TEST_SETS="$test_sets" TEST_LANG=$test_lang  utils/benchmark.sh
