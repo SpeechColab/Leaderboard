55408cba3064c169fc27ad3fc8d96397f6a42c7d
dophist <jerry.ejwt@qq.com>
2021-10-26 20:53:27 +0800

    add dos2unix for robustness

--- utils/benchmark.sh
+++ utils/benchmark.sh
@@ -43,6 +43,7 @@ for x in $test_sets; do
     if [ $stage -le 2 ]; then
         echo "$0 --> Recognizing in $dir"
         if [ -f 'SBI' ]; then
+            dos2unix *
             chmod +x SBI
             chmod +x *
             ./SBI $dir/wav.scp $dir >& $dir/log.SBI
