3ef31152a14d3c25e4443c2d54700cbf2b5ea3a7
lzr265946 <lzr265946@alibaba-inc.com>
2022-08-17 16:31:55 +0800

    add the -i and -k option for AK and AK secret

--- models/aliyun_ftasr_api_zh/del_oss_bucket.py
+++ models/aliyun_ftasr_api_zh/del_oss_bucket.py
@@ -21,10 +21,6 @@ if __name__ == "__main__":
     oss_config_path = sys.argv[1]
     test_time = sys.argv[2]
 
-    config_command = "./ossutil64 config -e oss-cn-hangzhou.aliyuncs.com -i {access_key_id} -k {access_key_secret} -c {oss_config_path}/myossconfig".format(access_key_id=access_key_id, access_key_secret=access_key_secret, oss_config_path=oss_config_path)
-    config_res = os.popen(config_command)
-    time.sleep(5)
-
     bucket = "oss://speechiotest-{test_time}".format(test_time=test_time)
-    del_command = "./ossutil64 rm {bucket} -b -a -r -f -e oss-cn-hangzhou.aliyuncs.com -c {oss_config_path}/myossconfig".format(bucket=bucket, oss_config_path=oss_config_path)
+    del_command = "./ossutil64 rm {bucket} -b -a -r -f -e oss-cn-hangzhou.aliyuncs.com -i {access_key_id} -k {access_key_secret}".format(bucket=bucket, access_key_id=access_key_id, access_key_secret=access_key_secret)
     del_res = os.popen(del_command)
--- models/aliyun_ftasr_api_zh/upload_wav.py
+++ models/aliyun_ftasr_api_zh/upload_wav.py
@@ -30,10 +30,6 @@ if __name__ == "__main__":
     oss_config_path = sys.argv[3]
     test_time = sys.argv[4]
 
-    config_command = "./ossutil64 config -e oss-cn-hangzhou.aliyuncs.com -i {access_key_id} -k {access_key_secret} -c {oss_config_path}/myossconfig".format(access_key_id=access_key_id, access_key_secret=access_key_secret, oss_config_path=oss_config_path)
-    config_res = os.popen(config_command)
-    time.sleep(5)
-
     # OSS bucket
     bucket = "oss://speechiotest-{test_time}/".format(test_time=test_time)
     for meta in wavFile:
@@ -42,7 +38,7 @@ if __name__ == "__main__":
         wavPath = meta[1]
         wavDict[wavId] = wavPath
     # 创建bucket
-    md_command = "./ossutil64 mb {bucket} -e oss-cn-hangzhou.aliyuncs.com -c {oss_config_path}/myossconfig".format(bucket=bucket, oss_config_path=oss_config_path)
+    md_command = "./ossutil64 mb {bucket} -e oss-cn-hangzhou.aliyuncs.com -i {access_key_id} -k {access_key_secret}".format(bucket=bucket, access_key_id=access_key_id, access_key_secret=access_key_secret)
     md_res = os.popen(md_command)
 
     time.sleep(5)
@@ -50,10 +46,10 @@ if __name__ == "__main__":
     ossWavFile = codecs.open(sys.argv[2], 'w+', 'utf8')
     for idx, wav in wavDict.items():
         # 上传文件
-        upload_command = "./ossutil64 cp {wav} {osspath} -e oss-cn-hangzhou.aliyuncs.com -c {oss_config_path}/myossconfig".format(wav=wav, osspath=ossPath, oss_config_path=oss_config_path)
+        upload_command = "./ossutil64 cp {wav} {osspath} -e oss-cn-hangzhou.aliyuncs.com -i {access_key_id} -k {access_key_secret}".format(wav=wav, osspath=ossPath, access_key_id=access_key_id, access_key_secret=access_key_secret)
         upload_res = os.popen(upload_command) 
         # 生成签名URL
-        sign_command = './ossutil64 sign {osspath}{idx}.wav --timeout {times} -e oss-cn-hangzhou.aliyuncs.com -c {oss_config_path}/myossconfig'.format(osspath=ossPath, idx=idx, times=32400, oss_config_path=oss_config_path)
+        sign_command = "./ossutil64 sign {osspath}{idx}.wav --timeout {times} -e oss-cn-hangzhou.aliyuncs.com -i {access_key_id} -k {access_key_secret}".format(osspath=ossPath, idx=idx, times=32400, access_key_id=access_key_id, access_key_secret=access_key_secret)
         res = os.popen(sign_command)
         for audio in res:
             ossWavFile.write(idx + "\t" + audio)
