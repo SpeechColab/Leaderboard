2b06a19a6e6be91d7c17188fb91dea029b3f7ce3
dophist <jerry.jiayu.du@gmail.com>
2022-10-08 10:25:21 +0800

    refine en textnorm

--- utils/textnorm_en.py
+++ utils/textnorm_en.py
@@ -26,9 +26,6 @@ if __name__ == '__main__':
     p.add_argument('--log_interval', type=int, default=10000, help='log interval in number of processed lines')
     args = p.parse_args()
 
-    ifile = open(args.ifile, 'r',  encoding = 'utf8')
-    ofile = open(args.ofile, 'w+', encoding = 'utf8')
-
     nemo_tn_en = Normalizer(input_case='lower_cased', lang='en')
 
     itj = read_interjections(
@@ -39,62 +36,63 @@ if __name__ == '__main__':
     certain_single_quote_items = ["\"'", "'?", "'!","'.", "?'", "!'", ".'","''", "<BOS>'", "'<EOS>"]
     single_quote_removed_items = [ x.replace("'", '') for x in certain_single_quote_items ]
 
+    puncts_to_remove = string.punctuation.replace("'", '')
+    puncts_trans = str.maketrans(puncts_to_remove, ' ' * len(puncts_to_remove), '')
+
     n = 0
-    for l in ifile:
-        key = ''
-        text = ''
-        if args.has_key:
-            cols = l.split(maxsplit=1)
-            key = cols[0]
-            if len(cols) == 2:
-                text = cols[1].strip()
+    with open(args.ifile, 'r',  encoding = 'utf8') as fi, open(args.ofile, 'w+', encoding = 'utf8') as fo:
+        for line in fi:
+            if args.has_key:
+                cols = line.strip().split(maxsplit=1)
+                key, text = cols[0], cols[1] if len(cols) == 2 else ''
             else:
-                text = ''
-        else:
-            text = l.strip()
+                text = line.strip()
 
-        # nemo text normalization
-        text = text.lower()
-        text = nemo_tn_en.normalize(text)
 
-        # Punctuations
-        # NOTE(2022.10 Jiayu):
-        # Single quote removal is not perfect.
-        # ' need to be reserved for:
-        #   abbreviations: 
-        #     I'm, don't, she'd, 'cause, Sweet Child o' Mine, Guns N' Roses, ...
-        #   possessions:
-        #     John's, the king's, parents', ...
-        text = '<BOS>' + text + '<EOS>'
-        for x, y in zip(certain_single_quote_items, single_quote_removed_items):
-            text = text.replace(x, y)
-        text = text.replace('<BOS>','').replace('<EOS>','')
+            # nemo text normalization
+            # modifications to NeMo:
+            # 1. added UK to US conversion: nemo_text_processing/text_normalization/en/data/whitelist/UK_to_US.tsv
+            # 2. swith 'oh' to 'o' in year TN to avoid confusion with interjections, e.g.:
+            #    1805: eighteen oh five -> eighteen o five
+            text = nemo_tn_en.normalize( text.lower() )
 
-        puncts_to_remove = string.punctuation.replace("'", '')
-        text = text.translate(str.maketrans(puncts_to_remove, ' '*len(puncts_to_remove), ''))
 
-        # Interjections
-        text = ' '.join([ x for x in text.strip().split() if x not in itj_map ])
+            # Punctuations
+            # NOTE(2022.10 Jiayu):
+            # Single quote removal is not perfect.
+            # ' needs to be reserved for:
+            #     Abbreviations:
+            #       I'm, don't, she'd, 'cause, Sweet Child o' Mine, Guns N' Roses, ...
+            #     Possessions:
+            #       John's, the king's, parents', ...
+            text = '<BOS>' + text + '<EOS>'
+            for x, y in zip(certain_single_quote_items, single_quote_removed_items):
+                text = text.replace(x, y)
+            text = text.replace('<BOS>','').replace('<EOS>','')
 
-        # Cases
-        if args.to_upper and args.to_lower:
-            sys.stderr.write('text norm: to_upper OR to_lower?')
-            exit(1)
-        if args.to_upper:
-            text = text.upper()
-        if args.to_lower:
-            text = text.lower()
+            text = text.translate(puncts_trans)
 
-        if args.has_key:
-            ofile.write(key + '\t' + text + '\n')
-        else:
-            ofile.write(text + '\n')
 
-        n += 1
-        if n % args.log_interval == 0:
-            sys.stderr.write("text norm: {} lines done.\n".format(n))
+            # Interjections
+            text = ' '.join([ x for x in text.strip().split() if x not in itj_map ])
 
-    sys.stderr.write("text norm: {} lines done in total.\n".format(n))
 
-    ifile.close()
-    ofile.close()
+            # Cases
+            if args.to_upper and args.to_lower:
+                sys.stderr.write('text norm: to_upper OR to_lower?')
+                exit(1)
+            if args.to_upper:
+                text = text.upper()
+            if args.to_lower:
+                text = text.lower()
+
+            if args.has_key:
+                print(key + '\t' + text, file = fo)
+            else:
+                print(text, file = fo)
+
+            n += 1
+            if n % args.log_interval == 0:
+                print(f'text norm: {n} lines done.', file = sys.stderr)
+    print(f'text norm: {n} lines done in total.', file = sys.stderr)
+
